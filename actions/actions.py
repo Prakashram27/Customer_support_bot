ec8376b829f2e56f094a11bbe26c925770ba3077
Prakash <prakashram1327@gmail.com>
2023-12-27 19:32:36 +0530

    fallback add, progress on ticket creation

--- actions/actions.py
+++ actions/actions.py
@@ -6,40 +6,58 @@ from rasa_sdk.executor import CollectingDispatcher
 import requests
 from pprint import pprint
 
-class ActionHelloWorld(Action):
+# class ActionHelloWorld(Action):
 
+#     def name(self) -> Text:
+#         return "action_human_handoff"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         dispatcher.utter_message(text="Please wait still we connect to agent.")
+
+#         conversation_id = int(tracker.sender_id)
+
+#         ##URL##
+#         url = f"http://localhost:3000/api/v1/accounts/1/conversations/{conversation_id}/toggle_status"
+
+#         ##DATA##
+#         data = {
+#             "status": "open"
+#         }
+#         ##HEADERS##
+#         headers = {
+#             "api_access_token": "4a36A81zfnmswZzjPVdr7drF",
+#             "Content-Type": "application/json"  # Adjust the content type as needed
+#         }
+#         ##REQUEST##
+#         response = requests.post(url, json=data, headers=headers,verify=False)
+
+#         # Check the response
+#         if response.status_code == 200:
+#             print("POST request was successful!")
+#             print("Response JSON:", response.json())
+#         else:
+#             print("POST request failed with status code:", response.status_code)
+#             print("Response content:", response.text)
+
+#         return []
+
+class ActionAskEmail(Action):
     def name(self) -> Text:
-        return "action_human_handoff"
+        return "action_ask_email"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Please wait still we connect to agent.")
-
-        conversation_id = int(tracker.sender_id)
-
-        ##URL##
-        url = f"http://localhost:3000/api/v1/accounts/1/conversations/{conversation_id}/toggle_status"
-
-        ##DATA##
-        data = {
-            "status": "open"
-        }
-        ##HEADERS##
-        headers = {
-            "api_access_token": "4a36A81zfnmswZzjPVdr7drF",
-            "Content-Type": "application/json"  # Adjust the content type as needed
-        }
-        ##REQUEST##
-        response = requests.post(url, json=data, headers=headers,verify=False)
-
-        # Check the response
-        if response.status_code == 200:
-            print("POST request was successful!")
-            print("Response JSON:", response.json())
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        if tracker.get_slot("previous_email"):
+            dispatcher.utter_message(template=f"utter_ask_use_previous_email",)
         else:
-            print("POST request failed with status code:", response.status_code)
-            print("Response content:", response.text)
-
+            dispatcher.utter_message(template=f"utter_ask_email")
         return []
+
+
